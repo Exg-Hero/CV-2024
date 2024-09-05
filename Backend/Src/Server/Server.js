0acda55cb2dedc12c1416e5093eab3afb600338a
sBlin <sblinkrst@outlook.com>
2024-09-04 21:47:53 -0600

    first commit3

--- Backend/Src/Server/Server.js
+++ Backend/Src/Server/Server.js
@@ -36,7 +36,12 @@ class Server {
         this.express.use(express.static('Static'));
         // Redirigir todas las rutas no manejadas a `index.html`
         this.express.get('/*', (req, res) => {
-            express.static(path.join(__dirname, '..', 'static', 'index.html'));        });
+            res.sendFile(path.resolve( '..', 'Backend_2024/Static', 'index.html'), (err) => {
+                if (err) {
+                    res.status(500).send(err);  // Si hay un error, envía un código de error 500
+                }
+            });});
+
     }
 
     async iniciarServidor() {
